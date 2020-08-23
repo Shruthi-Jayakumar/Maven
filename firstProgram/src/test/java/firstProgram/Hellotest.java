0bf3e1a731b1214634cac67d14a531d9aac54cbb
Shruthi Jayakumar <shruthijayakumar1996@gmail.com>
2020-08-23 11:42:40 +0530

    Second commit

--- firstProgram/src/test/java/firstProgram/Hellotest.java
+++ firstProgram/src/test/java/firstProgram/Hellotest.java
@@ -4,6 +4,7 @@ public class Hellotest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Maven Project Testing");
+		System.out.println("2ndline");
 
 }
 }
